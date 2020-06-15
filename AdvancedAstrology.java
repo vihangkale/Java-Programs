76d555eaa7c64bdee3ee177875652214b9abe07e
Vihang Kale <59720809+vihangkale@users.noreply.github.com>
2020-06-15 18:18:34 +0530

    Christmas tree part need some work.

--- AdvancedAstrology.java
+++ AdvancedAstrology.java
@@ -23,8 +23,7 @@ public class AdvancedAstrology {
         for(int i=1;i<=size;i++) {
 	
             for(int j=0;j<i;j++) {
-                System.out.print("*");
-                 
+                System.out.print("*");                 
             }
             System.out.println();
         }             
